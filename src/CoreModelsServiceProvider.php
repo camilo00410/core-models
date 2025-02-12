4daa70e8d5fcb40a63e6b3361c8033586bcd4960
camilo00410 <63594429+camilo00410@users.noreply.github.com>
2025-02-12 17:12:01 +0000

    Fix styling

--- src/CoreModelsServiceProvider.php
+++ src/CoreModelsServiceProvider.php
@@ -4,7 +4,6 @@ namespace VendorName\Skeleton;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use VendorName\Skeleton\Commands\SkeletonCommand;
 
 class CoreModelsServiceProvider extends PackageServiceProvider
 {
